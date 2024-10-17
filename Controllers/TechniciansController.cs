515ab8c2d8124edf9fbbdf6d3d9b9d6b26bcaa89
Scott <greatvstech@gmail.com>
2024-10-17 12:12:35 -0700

    Add TechniciansController and CRUD views for Technician
    Added a new `TechniciansController` class in `TechniciansController.cs` to handle CRUD operations for the `Technician` model. This includes methods for listing, viewing details, creating, editing, and deleting technicians.
    
    Created new views:
    - `Create.cshtml`: Form for creating a new technician.
    - `Delete.cshtml`: Confirmation for deleting a technician.
    - `Details.cshtml`: Display details of a technician.
    - `Edit.cshtml`: Form for editing an existing technician.
    - `Index.cshtml`: List all technicians with options to edit, view details, and delete.

---
+++ Controllers/TechniciansController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NailManagement.Data;
+using NailManagement.Models;
+
+namespace NailManagement.Controllers
+{
+    public class TechniciansController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TechniciansController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Technicians
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Technicians.ToListAsync());
+        }
+
+        // GET: Technicians/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var technician = await _context.Technicians
+                .FirstOrDefaultAsync(m => m.TechnicianId == id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+
+            return View(technician);
+        }
+
+        // GET: Technicians/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Technicians/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TechnicianId,Specialties,Rating,ProfilePicture,FirstName,LastName,Email,PhoneNumber,DateOfBirth")] Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(technician);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(technician);
+        }
+
+        // GET: Technicians/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var technician = await _context.Technicians.FindAsync(id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+            return View(technician);
+        }
+
+        // POST: Technicians/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TechnicianId,Specialties,Rating,ProfilePicture,FirstName,LastName,Email,PhoneNumber,DateOfBirth")] Technician technician)
+        {
+            if (id != technician.TechnicianId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(technician);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TechnicianExists(technician.TechnicianId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(technician);
+        }
+
+        // GET: Technicians/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var technician = await _context.Technicians
+                .FirstOrDefaultAsync(m => m.TechnicianId == id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+
+            return View(technician);
+        }
+
+        // POST: Technicians/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var technician = await _context.Technicians.FindAsync(id);
+            if (technician != null)
+            {
+                _context.Technicians.Remove(technician);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TechnicianExists(int id)
+        {
+            return _context.Technicians.Any(e => e.TechnicianId == id);
+        }
+    }
+}
